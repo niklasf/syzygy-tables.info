766ea7b5019d4de29c360d9ddce6e22aa3bfbae7
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-03-17 15:48:25 +0100

    Make h1 a home button

--- static/script.js
+++ static/script.js
@@ -389,5 +389,14 @@ $(function () {
     probe(fen, false);
   });
 
+  $('h1 a').click(function (event) {
+    event.preventDefault();
+    $fen.val('');
+    board.position('4k3/8/8/8/8/8/8/4K3 w - - 0 1');
+    $btn_white.addClass('active');
+    $btn_black.removeClass('active');
+    probe('4k3/8/8/8/8/8/8/4K3 w - - 0 1', true);
+  });
+
   $('.list-group-item').click(handleMoveClick);
 });
