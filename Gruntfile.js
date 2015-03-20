d99677affb6eb540e192c92197e090cc70db6515
Niklas Fiekas <niklas.fiekas@tu-clausthal.de>
2015-03-20 18:41:43 +0100

    Provide sourcemap to individual js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,19 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      js: {
-        src: ['src/jquery-2.1.3.js', 'src/chess.js', 'src/chessboard-0.3.0.js', 'src/client.js'],
-        dest: 'static/client.js'
-      },
-    },
     uglify: {
       options: {
         compress: true,
         mangle: true,
         sourceMap: true
       },
-      build: {
-        src: 'static/client.js',
-        dest: 'static/client.min.js'
+      target: {
+        files: {
+          'static/client.min.js': ['static/jquery-2.1.3.js', 'static/chess.js', 'static/chessboard-0.3.0.js', 'static/client.js']
+        }
       }
     },
     cssmin: {
@@ -35,5 +29,5 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+  grunt.registerTask('default', ['uglify', 'cssmin']);
 };
