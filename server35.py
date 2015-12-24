9a5ee26709e0d432be21ad30ef78ba743ac24dd2
Niklas Fiekas <niklas.fiekas@backscattering.de>
2015-12-24 19:02:52 +0100

    Add sitemap route

--- server35.py
+++ server35.py
@@ -5,7 +5,10 @@ import os
 import chess
 import chess.syzygy
 import chess.gaviota
+import chess.pgn
 import json
+import logging
+import random
 
 
 def static(url, path):
@@ -28,9 +31,13 @@ def jsonp(request, obj):
     callback = request.GET.get("callback")
     if callback:
         content = "%s(%s);" % (callback, json_str)
-        return aiohttp.web.Response(text=content, content_type="application/javascript")
+        return aiohttp.web.Response(
+            text=content,
+            content_type="application/javascript")
     else:
-        return aiohttp.web.Response(text=json_str, content_type="text/json")
+        return aiohttp.web.Response(
+            text=json_str,
+            content_type="text/json")
 
 
 class Api(object):
@@ -47,23 +54,24 @@ class Api(object):
         self.syzygy = chess.syzygy.Tablebases()
 
         for line in config.get("tablebases", "syzygy").splitlines():
-            filename = line.strip()
-            if not filename:
+            path = line.strip()
+            if not path:
                 continue
 
-            num = self.syzygy.open_directory(filename)
-            print("* Loaded %d syzygy tablebases from %r" % (num, filename))
+            num = self.syzygy.open_directory(path)
+            print("* Loaded %d syzygy tablebases from %r" % (num, path))
 
     def init_gaviota(self):
         print("Loading gaviota tablebases ...")
         self.gaviota = chess.gaviota.open_tablebases()
 
         for line in config.get("tablebases", "gaviota").splitlines():
-            filename = line.strip()
-            if not filename:
+            path = line.strip()
+            if not path:
                 continue
 
-            print("* Loaded gaviota tablebases from %r" % filename)
+            self.gaviota.open_directory(path)
+            print("* Loaded gaviota tablebases from %r" % path)
 
     def probe(self, board, load_root=False, load_wdl=False, load_dtz=False, load_dtm=False):
         result = {}
@@ -93,7 +101,7 @@ class Api(object):
 
         return result
 
-    async def probe_async(self, board, load_root=False, load_wdl=False, load_dtz=False, load_dtm=False):
+    async def probe_async(self, board, *, load_root=False, load_wdl=False, load_dtz=False, load_dtm=False):
         return await self.loop.run_in_executor(
             None,
             self.probe, board.copy(), load_root, load_wdl, load_dtz, load_dtm)
@@ -126,36 +134,69 @@ class Api(object):
         result = await self.probe_async(board, load_root=True, load_dtz=True, load_dtm=True)
         return jsonp(request, result)
 
+    async def pgn(self, request):
+        board = chess.Board()
+
+        game = chess.pgn.Game()
+        node = game
+
+        for _ in range(100):
+            await self.probe_async(board, load_root=True, load_dtz=True)
+            move = random.choice(list(board.legal_moves))
+            node = node.add_variation(move)
+            board.push(move)
+
+        game.headers["Result"] = board.result()
+
+        return aiohttp.web.Response(text=str(game))
+
+
+def sitemap(config):
+    entries = [
+        "",
+        "?fen=6N1/5KR1/2n5/8/8/8/2n5/1k6%20w%20-%20-%200%201",
+        "?fen=4r3/1K6/8/8/5p2/3k4/8/7Q%20b%20-%20-%200%201",
+        "apidoc?fen=6N1/5KR1/2n5/8/8/8/2n5/1k6%20w%20-%20-%200%201",
+        "legal",
+    ]
+
+    content = "\n".join("https://syzygy-tables.info/" + entry for entry in entries)
+    return aiohttp.web.Response(text=content)
+
 
 async def init(config, loop):
+    print("---")
+
     api = Api(config, loop)
 
     # Setup routes.
     app = aiohttp.web.Application(loop=loop)
     app.router.add_route("GET", "/favicon.ico", static("/favicon.ico", "favicon.ico"))
+    app.router.add_route("GET", "/sitemap.txt", sitemap)
     app.router.add_route("GET", "/api/v1", api.v1)
     app.router.add_route("GET", "/api/v2", api.v2)
+    app.router.add_route("GET", "/api/pgn", api.pgn)
 
     # Create server.
     bind = config.get("server", "bind")
     port = config.getint("server", "port")
     server = await loop.create_server(app.make_handler(), bind, port)
     print("Listening on http://%s:%d/ ..." % (bind, port))
+
+    print("---")
     return server
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     loop = asyncio.get_event_loop()
 
     config = configparser.ConfigParser()
 
     config.read([
-        os.path.join(os.path.dirname(__file__), "config.ini"),
         os.path.join(os.path.dirname(__file__), "config.default.ini"),
+        os.path.join(os.path.dirname(__file__), "config.ini"),
     ])
 
-    try:
-        loop.run_until_complete(init(config, loop))
-        loop.run_forever()
-    finally:
-        loop.close()
+    loop.run_until_complete(init(config, loop))
+    loop.run_forever()
