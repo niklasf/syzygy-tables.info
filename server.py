5571a44bc7ac6f36921d04bf46ce5f052b9654f9
Niklas Fiekas <niklas.fiekas@backscattering.de>
2018-04-08 11:09:06 +0200

    add disclaimer about experimental tables

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # This file is part of the syzygy-tables.info tablebase probing website.
-# Copyright (C) 2015-2017 Niklas Fiekas <niklas.fiekas@backscattering.de>
+# Copyright (C) 2015-2018 Niklas Fiekas <niklas.fiekas@backscattering.de>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -361,6 +361,7 @@ class Frontend(object):
 
         # Material key for the page title.
         render["material"] = material(board)
+        render["piece_count"] = chess.popcount(board.occupied)
 
         # Moves are going to be grouped by WDL.
         grouped_moves = {-2: [], -1: [], 0: [], 1: [], 2: [], None: []}
