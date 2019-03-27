bae03be499a459a64aa90aa0060f492af78e9365
Niklas Fiekas <niklas.fiekas@backscattering.de>
2019-03-27 18:02:51 +0100

    add maxdtz.pgn

--- server.py
+++ server.py
@@ -562,6 +562,7 @@ def make_app(config):
     # Setup routes.
     app.router.add_routes(routes)
     app.router.add_static("/static", "static")
+    app.router.add_route("GET", "/endgames.pgn", static("stats/regular/maxdtz.pgn"))
     app.router.add_route("GET", "/stats.json", static("stats.json"))
     return app
 
