niklasf/syzygy-tables.info

commit 3070242cf6c7fb96b95e00f8f5b06840839ae4be

update stats file format

server.py
@@ -91,12 +91,12 @@ def prepare_stats(request, material, fen, active_dtz):
 
     # Get stats and side.
     stats = request.app["stats"].get(material)
-    side = "w"
-    other = "b"
+    side = "white"
+    other = "black"
     if stats is None:
         stats = request.app["stats"].get(chess.syzygy.normalize_tablename(material))
-        side = "b"
-        other = "w"
+        side = "black"
+        other = "white"
     if stats is None:
         return None
 
@@ -104,11 +104,11 @@ def prepare_stats(request, material, fen, active_dtz):
 
     # Basic statistics.
     outcomes = {
-        "white": stats[side]["wdl"]["2"] + stats[other]["wdl"]["-2"],
-        "cursed": stats[side]["wdl"]["1"] + stats[other]["wdl"]["-1"],
-        "draws": stats[side]["wdl"]["0"] + stats[other]["wdl"]["0"],
-        "blessed": stats[side]["wdl"]["-1"] + stats[other]["wdl"]["1"],
-        "black": stats[side]["wdl"]["-2"] + stats[other]["wdl"]["2"],
+        "white": stats["histogram"][side]["wdl"]["2"] + stats["histogram"][other]["wdl"]["-2"],
+        "cursed": stats["histogram"][side]["wdl"]["1"] + stats["histogram"][other]["wdl"]["-1"],
+        "draws": stats["histogram"][side]["wdl"]["0"] + stats["histogram"][other]["wdl"]["0"],
+        "blessed": stats["histogram"][side]["wdl"]["-1"] + stats["histogram"][other]["wdl"]["1"],
+        "black": stats["histogram"][side]["wdl"]["-2"] + stats["histogram"][other]["wdl"]["2"],
     }
 
     total = sum(outcomes.values())
@@ -133,8 +133,8 @@ def prepare_stats(request, material, fen, active_dtz):
     side_winning = (" w" in fen) == (active_dtz is not None and active_dtz > 0)
     render["verb"] = "winning" if side_winning else "losing"
 
-    win_hist = stats[side]["win_hist" if side_winning else "loss_hist"]
-    loss_hist = stats[other]["loss_hist" if side_winning else "win_hist"]
+    win_hist = stats["histogram"][side]["win" if side_winning else "loss"]
+    loss_hist = stats["histogram"][other]["loss" if side_winning else "win"]
     hist = [a + b for a, b in itertools.zip_longest(win_hist, loss_hist, fillvalue=0)]
     if not any(hist):
         return render

templates/stats.html
@@ -36,37 +36,22 @@
     <h2>Example (KRNvKNN)</h2>
 
     <pre><code class="language-javascript">{
-  "w": { // white to move
-    "win_hist": [
-      0,
-      1924310948,
-      35087,
-      363845772,
-      37120,
-      138550471,
-      // ...
-    ]
-    "loss_hist": [
-      98698,
-      144,
-      3810,
-      0,
-      596,
-      0,
-      58
-    ],
-    "wdl": {
-      "-2": 0,
-      "-1": 103306,
-      "0": 1333429189,
-      "1": 162344388,
-      "2": 2959977091
-    }
+  "rtbw": {
+    "bytes": 290002640, // file size
+    "tbcheck": "a320ac...", // internal checksum
+    "md5": "6ee435...",
+    "sha1": "07a0e4...",
+    "sha256": "f3386d...",
+    "sha512": "c4bf73...",
+    "b2": "b970e0...", // blake 2
+    "ipfs": "QmXW4S..."
   },
-  "b": { // black to move
+  "rtbz": {
     // ...
   },
   "longest": [
+    // longest winning endgames for black/white
+    // with/without 50-move rule
     {
       "epd": "3n1n2/8/8/8/4R3/8/8/NK1k4 b - -",
       "ply": 100,
@@ -82,22 +67,38 @@
       "ply": 7,
       "wdl": 2
     }
-  ]
+  ],
+  "histogram": {
+    "white": { // white to move
+      "win": [
+        0,
+        1924310948,
+        35087,
+        363845772,
+        37120,
+        138550471,
+        // ...
+      ]
+      "loss": [
+        98698, // # of positions losing in 0
+        144, // # of positions losing in 1
+        3810, // # of positions losing in 2
+        0, // 3
+        596, // 4
+        0, // 5
+        58 // 6
+      ],
+      "wdl": { // # of positions with each wdl
+        "-2": 0,
+        "-1": 103306,
+        "0": 1333429189,
+        "1": 162344388,
+        "2": 2959977091
+      }
+    },
+    "b": { // black to move
+      // ...
+    },
+  }
 }</code></pre>
-    <p>
-      Information for <code>w</code> and <code>b</code> to move respectively.
-    </p>
-    <p>
-      Histograms <code>win_hist</code> and <code>loss_hist</code>: Number of
-      positions winning/losing in 0, 1, 2, ... plies until the next pawn move
-      or capture.
-    </p>
-    <p>
-      Number of positions with <code>wdl</code>.
-    </p>
-    <p>
-      <code>longest</code> endgames with white and black winning
-      and winning except for the fifty-move rule, if such endgames exist.
-    </p>
-  </section>
 {% endblock %}

commit 3e4b063b786f2c835d745086dde70d7b5ef41343

fix winning/losing labels for longest endgames (fixes #85)
broken in 3070242cf6c7fb96b95e00f8f5b06840839ae4be

server.py
@@ -125,7 +125,7 @@ def prepare_stats(request, material, fen, active_dtz):
     render["longest"] = [{
         "label": "{} {} with DTZ {}{}".format(
             material_side,
-            "winning" if (longest["wdl"] > 0) == ((" " + side) in longest["epd"]) else "losing",
+            "winning" if (longest["wdl"] > 0) == ((" " + side[0]) in longest["epd"]) else "losing",
             longest["ply"],
             " (frustrated)" if abs(longest["wdl"]) == 1 else ""),
         "fen": longest["epd"] + " 0 1",