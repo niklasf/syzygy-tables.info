d97f76f7618e42a4a11b4fb1a6ad085eb3c0f91d
Niklas Fiekas <niklas.fiekas@backscattering.de>
2026-02-10 20:38:58 +0100

    more words on illegal positions

--- syzygy_tables_info/server.py
+++ syzygy_tables_info/server.py
@@ -592,7 +592,12 @@ async def index(request: aiohttp.web.Request) -> aiohttp.web.Response:
         if request.app["tarpit"] and "fen" in request.query:
             await asyncio.sleep(min(random.lognormvariate(1.1, 0.4), 9))
             rng = random.Random(request.query["fen"])
-            render["tarpit"] = syzygy_tables_info.tarpit.MARKOV_CHAIN.generate_text(12 + rng.randint(0, 1 + 20 * sum(len(g) for g in grouped_moves.values())), rng)
+            num_words = 12
+            if render["illegal"]:
+                num_words += 200
+            else:
+                num_words = rng.randint(0, 1 + 20 * sum(len(g) for g in grouped_moves.values()))
+            render["tarpit"] = syzygy_tables_info.tarpit.MARKOV_CHAIN.generate_text(num_words, rng)
 
         html = syzygy_tables_info.views.index(
             development=request.app["development"], render=render
